037813e6fc0b9be1134b87a0ae325a4f955164f0
johanngerberding <johann.gerberding@lector.ai>
2025-12-02 08:14:39 +0100

    part 2 day 1

--- problems/01/main.cpp
+++ problems/01/main.cpp
@@ -25,29 +25,57 @@ std::vector<std::string> loadFromFile(const std::string& filename) {
 }
 
 
-int countZeros(const std::vector<std::string>& rotations, int start) {
+int countZeros(const std::vector<std::string>& rotations, int start, int part) {
     int zeros {0};
+    int overrun {0};
     for (const auto& rotation: rotations) {
         int rot = std::stoi(rotation.substr(1));
         if (rotation[0] == 'L') {
-            start -= rot;
-            while (start < 0) {
-                start = 99 + start + 1;
+            if (rot > start) {
+                if (start == 0) {
+                    zeros--;
+                } 
+                start -= rot;
+                while (start < 0) {
+                    overrun++; 
+                    if (part == 2) {
+                        zeros++;
+                    }
+                    start = 100 + start;
+                }
+                if (start == 0) {
+                    zeros++;
+                }
+            } else if (rot == start) {
+                start = 0;
+                zeros++;
+            } else {
+                start -= rot;
             }
+            std::cout << rotation << " caused " << overrun << " overruns. curr: " << start << " zero count: " << zeros << std::endl;
         } else if (rotation[0] == 'R') {
-            start += rot;
-            while (start > 99) {
-                start = start - 100;
+            if (rot > (100 - start)) { 
+                start += rot;
+                while (start > 99) {
+                    overrun++;
+                    if (part == 2) {
+                        zeros++;
+                    }
+                    start = start - 100;
+                }
+            } else if (rot == (100 - start)) {
+                start = 0;
+                zeros++;
+            } else {
+                start += rot;
             }
+            std::cout << rotation << " caused " << overrun << " overruns. curr: " << start << " zero count: " << zeros << std::endl;
         } else {
             throw std::invalid_argument("Invalid first character in rotation " + rotation);            
         }
 
-        // std::cout << start << std::endl;
+        overrun = 0;
 
-        if (start == 0) {
-            zeros++;
-        }
     } 
     return zeros;
 }
@@ -56,10 +84,16 @@ int main() {
     std::vector<std::string> test_rotations = {
         "L68", "L30", "R48", "L5", "R60", "L55", "L1", "L99", "R14", "L82"
     };
-    int test_zeros = countZeros(test_rotations, 50);
-    std::cout << "Test zero count: " << test_zeros << std::endl;
+    // int test_zeros = countZeros(test_rotations, 50, 1);
+    // std::cout << "Test zero count: " << test_zeros << std::endl;
+    // std::vector<std::string> rotations = loadFromFile("input.txt");
+    // int zeros = countZeros(rotations, 50, 1);
+    // std::cout << "Zero count: " << zeros << std::endl;
+    
+    int test_zeros = countZeros(test_rotations, 50, 2);
+    std::cout << "Part 2 zero count: " << test_zeros << std::endl;
     std::vector<std::string> rotations = loadFromFile("input.txt");
-    int zeros = countZeros(rotations, 50);
-    std::cout << "Zero count: " << zeros << std::endl;
-    return 0;
+    int zeros = countZeros(rotations, 50, 2);
+    std::cout << "Part 2 Zero count: " << zeros << std::endl;
+   return 0;
 }
