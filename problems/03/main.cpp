55dccce1211ab0f1873f361f3223ad59b159c110
johanngerberding <johann.gerberding@lector.ai>
2025-12-04 07:29:39 +0100

    day 3 part 2

--- problems/03/main.cpp
+++ problems/03/main.cpp
@@ -36,31 +36,71 @@ std::vector<std::vector<int>> loadFromFile(std::string filename) {
     return ids;
 }
 
+
+long long buildNumber(std::vector<int> numbers, int n) {
+    std::vector<int> batteries(numbers.begin(), numbers.begin() + n);
+
+    for (size_t idx = 12; idx < numbers.size(); idx++) {
+        int joltage = numbers[idx];
+
+        bool inserted = false; 
+        for (size_t i = 0; i < 11; ++i) {
+            if (batteries[i] < batteries[i + 1]) {
+                batteries.erase(batteries.begin() + i);
+                batteries.push_back(joltage);
+                inserted = true;
+                break;
+            } 
+        }
+
+        if (!inserted && joltage > batteries.back()) {
+            batteries.back() = joltage;
+        }
+    }
+    
+    std::string result;
+    for (int b: batteries) {
+        result += std::to_string(b);
+    }
+    std::cout << result << std::endl; 
+    return std::stoull(result);
+}
+
 int main() {
     std::string filename = "input.txt";
     std::vector<std::vector<int>> lines = loadFromFile(filename);
-    std::vector<int> results; 
-    int result;
+    // std::vector<int> results; 
+    // int result;
+    // for (std::vector<int> line : lines) {
+    //     auto max_it = std::max_element(line.begin(), line.end()); 
+    //     int maxValue = *max_it;
+
+    //     int maxIndex = std::distance(line.begin(), max_it);
+    //     int maxValue2;         
+
+    //     // max element is last element
+    //     if (maxIndex == (line.size() - 1)) {
+    //         maxValue2 = maxValue; 
+    //         max_it = std::max_element(line.begin(), line.end() - 1);
+    //         maxValue = *max_it;
+    //     } else {
+    //         auto max2_it = std::max_element(max_it + 1, line.end());
+    //         maxValue2 = *max2_it;
+    //     } 
+    //     std::cout << "Max value: " << maxValue << " second " << maxValue2 << std::endl;
+    //     result = std::stoi((std::to_string(maxValue) + std::to_string(maxValue2)));
+    //     results.push_back(result);
+    // } 
+    // int sum = std::accumulate(results.begin(), results.end(), 0);
+    // std::cout << "Part 1: " << sum << std::endl;
+
+    std::vector<unsigned long long> part2_numbers; 
     for (std::vector<int> line : lines) {
-        // std::sort(line.rbegin(), line.rend());
-        auto max_it = std::max_element(line.begin(), line.end()); 
-        int maxValue = *max_it;
-        int maxIndex = std::distance(line.begin(), max_it);
-        int maxValue2;         
-        // max element is last element
-        if (maxIndex == (line.size() - 1)) {
-            maxValue2 = maxValue; 
-            max_it = std::max_element(line.begin(), line.end() - 1);
-            maxValue = *max_it;
-        } else {
-            auto max2_it = std::max_element(max_it + 1, line.end());
-            maxValue2 = *max2_it;
-        } 
-        std::cout << "Max value: " << maxValue << " second " << maxValue2 << std::endl;
-        result = std::stoi((std::to_string(maxValue) + std::to_string(maxValue2)));
-        results.push_back(result);
-    } 
-    int sum = std::accumulate(results.begin(), results.end(), 0);
-    std::cout << sum << std::endl;
+        part2_numbers.push_back(buildNumber(line, 12));
+    }
+
+    unsigned long long sum2 = std::accumulate(part2_numbers.begin(), part2_numbers.end(), 0LL);
+    std::cout << "Part 2: " << sum2 << std::endl;
+
     return 0;
 }
