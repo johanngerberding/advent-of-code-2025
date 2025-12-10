2ab36098a85ab82a5254426f0b533cb53e3a98f1
johanngerberding <johann.gerberding@lector.ai>
2025-12-10 21:34:12 +0100

    i am done for today

--- problems/08/main.cpp
+++ problems/08/main.cpp
@@ -4,6 +4,8 @@
 #include <fstream>
 #include <sstream>
 #include <cmath>
+#include <map> 
+#include <set> 
 
 
 struct Junction {
@@ -36,10 +38,63 @@ int main() {
         junctions.push_back(parseJunction(line));
     }
     file.close(); 
-
-    for (const auto& j: junctions) {
-        std::cout << "X: " << j.X << ", Y: " << j.Y << ", Z: " << j.Z << std::endl;
+    
+    size_t n = junctions.size();
+    std::vector<std::vector<double>> distances(n, std::vector<double>(n, 0.0));  
+    for (size_t i = 0; i < n; ++i) {
+        for (size_t j = 0; j < n; ++j) {
+            distances[i][j] = distance(junctions[i], junctions[j]);
+        }
     }
+    
+    std::vector<std::set<int>> circuits; 
+    std::set<std::pair<int, int>> done;
+
+    for (int round = 0; round < 10; round++) {
+        int min_i {-1};
+        int min_j {-1};
+        double min_distance {10000.0}; 
+        for (size_t i = 0; i < n; ++i) {
+            for (size_t j = 0; j < n; ++j) {
+                if (i != j && done.find(std::pair(i, j)) == done.end()) {
+                    if (distances[i][j] < min_distance) {
+                        min_distance = distances[i][j];
+                        min_i = i;
+                        min_j = j;
+                    }
+                } 
+            }
+        }
+        std::set<int> circuit; 
+        bool found = false;
+        if (circuits.empty()) {
+            circuit.insert(min_i);
+            circuit.insert(min_j);
+        } else {
+            
+            for (std::set<int> c: circuits) {
+                if (c.find(min_i) != c.end() || c.find(min_j) != c.end()) {
+                    c.insert(min_i);
+                    c.insert(min_j);
+                    found = true;
+                }
+            }
+            if (!found) {
+                circuit.insert(min_i);
+                circuit.insert(min_j);
+                circuits.push_back(circuit);
+            }
+        }
+        done.insert(std::pair(min_i, min_j));
+    }
+
+    for (std::set<int> c: circuits) {
+        std::cout << " circuit: "; 
+        for (int idx: c) {
+            std::cout << idx << " ";
+        }
+        std::cout << std::endl;
+    } 
 
     return 0;
 }
